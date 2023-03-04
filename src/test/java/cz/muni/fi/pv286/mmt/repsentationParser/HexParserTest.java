7f51dd731e65e16faf3bc73d17562736c64dd7f8
Daniel Múčka <daniel.mucka@outlook.sk>
2023-03-04 21:08:20 +0100

    feat: more hex tests

--- src/test/java/cz/muni/fi/pv286/mmt/repsentationParser/HexParserTest.java
+++ src/test/java/cz/muni/fi/pv286/mmt/repsentationParser/HexParserTest.java
@@ -9,6 +9,36 @@ class HexParserTest extends ParserTest {
     @Test
     public void roundTripTest() {
         assertRoundTrip(HexParser.class, "74657374".getBytes());
+        assertRoundTrip(HexParser.class, "7465".getBytes());
+        assertRoundTrip(HexParser.class, "74".getBytes());
+        assertRoundTrip(HexParser.class, "aa".getBytes());
+        assertRoundTrip(HexParser.class, "bb".getBytes());
+    }
+
+    @Test
+    public void roundTripUppercaseLettersTest() {
+        assertRoundTripFails(HexParser.class, "AA".getBytes());
+        assertRoundTripFails(HexParser.class, "BB".getBytes());
+
+        // note: uppercase letters cannot round trip, because the output is always lowercase
+        // this behaviour is not defined in the specification
+    }
+
+    @Test
+    public void invalidHexLengthTest() {
+        assertRoundTripThrows(HexParser.class, "7465737".getBytes());
+        assertRoundTripThrows(HexParser.class, "a".getBytes());
+        assertRoundTripThrows(HexParser.class, "aaa".getBytes());
+        assertRoundTripThrows(HexParser.class, "aaaaa".getBytes());
+    }
+
+    @Test
+    public void invalidHexCharactersTest() {
+        assertRoundTripThrows(HexParser.class, "test".getBytes());
+        assertRoundTripThrows(HexParser.class, "7465737g".getBytes());
+        assertRoundTripThrows(HexParser.class, "abcdefgh".getBytes());
+        assertRoundTripThrows(HexParser.class, "1234556x".getBytes());
+        assertRoundTripThrows(HexParser.class, "x1234567".getBytes());
     }
 
     @Test
--- src/test/java/cz/muni/fi/pv286/mmt/repsentationParser/ParserTest.java
+++ src/test/java/cz/muni/fi/pv286/mmt/repsentationParser/ParserTest.java
@@ -4,10 +4,50 @@ import cz.muni.fi.pv286.mmt.model.Options;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
+
+    private <From extends RepresentationParser, To extends RepresentationParser>
+    void assertConversionThrows(Options options, From fromParser, To toParser, byte[] input, byte[] output) {
+
+        var inputStream = new ByteArrayInputStream(input);
+        options.setInputFile(inputStream);
+
+        var outputStream = new ByteArrayOutputStream();
+        options.setOutputFile(outputStream);
+
+        try {
+            byte[] bytes = fromParser.parseFrom();
+            toParser.parseTo(bytes);
+        } catch (Exception e) {
+            assertTrue(true, e.getMessage());
+            return;
+        }
+
+        fail("Expected exception was not thrown.");
+    }
+
+    protected <From extends RepresentationParser, To extends RepresentationParser>
+    void assertConversionThrows(Class<From> fromClass, Class<To> toClass, byte[] input, byte[] output) {
+        Options options = new Options();
+
+        try {
+            From fromInstance = fromClass.getConstructor(Options.class).newInstance(options);
+            To toInstance = toClass.getConstructor(Options.class).newInstance(options);
+
+            assertConversionThrows(options, fromInstance, toInstance, input, output);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    protected <T extends RepresentationParser>
+    void assertRoundTripThrows(Class<T> parser, byte[] input) {
+        assertConversionThrows(parser, parser, input, input);
+    }
 
     private <From extends RepresentationParser, To extends RepresentationParser>
     void assertConversion(Options options, From fromParser, To toParser, byte[] input, byte[] output) {
@@ -45,5 +85,43 @@ public class ParserTest {
     protected <T extends RepresentationParser>
     void assertRoundTrip(Class<T> parser, byte[] input) {
         assertConversion(parser, parser, input, input);
+    }
+
+    private <From extends RepresentationParser, To extends RepresentationParser>
+    void assertConversionFails(Options options, From fromParser, To toParser, byte[] input, byte[] output) {
+
+        var inputStream = new ByteArrayInputStream(input);
+        options.setInputFile(inputStream);
+
+        var outputStream = new ByteArrayOutputStream();
+        options.setOutputFile(outputStream);
+
+        try {
+            byte[] bytes = fromParser.parseFrom();
+            toParser.parseTo(bytes);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        assertFalse(Arrays.equals(output, outputStream.toByteArray()));
+    }
+
+    protected <From extends RepresentationParser, To extends RepresentationParser>
+    void assertConversionFails(Class<From> fromClass, Class<To> toClass, byte[] input, byte[] output) {
+        Options options = new Options();
+
+        try {
+            From fromInstance = fromClass.getConstructor(Options.class).newInstance(options);
+            To toInstance = toClass.getConstructor(Options.class).newInstance(options);
+
+            assertConversionFails(options, fromInstance, toInstance, input, output);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    protected <T extends RepresentationParser>
+    void assertRoundTripFails(Class<T> parser, byte[] input) {
+        assertConversionFails(parser, parser, input, input);
     }
 }
