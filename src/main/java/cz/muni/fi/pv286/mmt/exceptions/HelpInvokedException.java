b4d91b1b29c3172668a61af38904f051886e3dc9
Daniel Múčka <daniel.mucka@outlook.sk>
2023-04-09 18:47:05 +0200

    Create review branch

--- src/main/java/cz/muni/fi/pv286/mmt/exceptions/HelpInvokedException.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pv286.mmt.exceptions;
-
-/**
- * This exception is used for showing help.
- */
-public class HelpInvokedException extends Exception {
-}
--- src/main/java/cz/muni/fi/pv286/mmt/model/FromOrTo.java
+++
@@ -1,9 +0,0 @@
-package cz.muni.fi.pv286.mmt.model;
-
-/**
- * Enum for From or To input parameters.
- */
-public enum FromOrTo {
-    FROM,
-    TO
-}
